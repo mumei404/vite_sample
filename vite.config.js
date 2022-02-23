f892a5ae1fb9e141f7093f3e681236be67ec171d
mumei404 <lozld30@gmail.com>
2022-02-24 04:35:03 +0900

    config legacy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite'
 
-export default {
-	plugins: [vue()]
-}
+export default defineConfig({
+	plugins: [
+		vue(),
+		legacy({
+			targets: ['ie >= 11'],
+			additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+		})
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `assets/eshima[name].js`,
+				chunkFileNames: `assets/cshima[name].js`,
+				assetFileNames: `assets/ashima[name].[ext]`
+			}
+		}
+	}
+})
