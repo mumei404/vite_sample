48f1f360790cb7afb58bbc3aa6d003d20e726c6e
mumei404 <lozld30@gmail.com>
2022-02-24 07:45:25 +0900

    for cake

---
+++ webroot/build/js/Article/main-legacy.96c71e93.js
@@ -0,0 +1,1 @@
+System.register([],(function(){"use strict";return{execute:function(){function e(){console.log("article sub")}e(),e(),e(),e()}}}));
---
+++ webroot/build/js/Article/main.3e13cb3c.js
@@ -0,0 +1,1 @@
+function t(){import("data:text/javascript,")}function o(){console.log("article sub")}o();o();o();o();export{t as __vite_legacy_guard};
